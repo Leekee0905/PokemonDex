98d89ca4aab82eec7eb5c1eae256ed52dc459fb1
GiSeong Lee <cj8928@gmail.com>
2024-08-26 19:19:04 +0900

    refactor: handler context로 이동

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -11,36 +11,7 @@ import usePokemon from "../hooks/usePokemon";
 
 const PokemonCard = ({ pokemonData, isSelect }) => {
   const navigate = useNavigate();
-  const { selectedPokemon, setSelectedPokemon } = usePokemon();
-
-  const handleAddButton = () => {
-    const dataLength = selectedPokemon.filter((e) => e !== null).length;
-    if (dataLength >= 6) {
-      alert("선택할 수 있는 포켓몬 수를 넘었습니다.");
-      return;
-    }
-    if (selectedPokemon.includes(pokemonData)) {
-      alert("이미 선택한 포켓몬입니다.");
-      return;
-    }
-    const newArr = [...selectedPokemon];
-    const nullIndex = newArr.indexOf(null);
-    if (nullIndex !== -1) {
-      newArr[nullIndex] = pokemonData;
-    }
-    setSelectedPokemon(newArr);
-  };
-
-  const handleDeleteButton = (id) => {
-    const filteredData = selectedPokemon.filter(
-      (e) => e !== null && e.id !== id
-    );
-
-    while (filteredData.length < selectedPokemon.length) {
-      filteredData.push(null);
-    }
-    setSelectedPokemon(filteredData);
-  };
+  const { handleAddButton, handleDeleteButton } = usePokemon();
 
   const convertPokemonId = (id) => {
     return id.toString().padStart(3, "0");
@@ -63,7 +34,9 @@ const PokemonCard = ({ pokemonData, isSelect }) => {
         fontSize="16px"
         onClick={(e) => {
           e.stopPropagation();
-          isSelect ? handleDeleteButton(pokemonData.id) : handleAddButton();
+          isSelect
+            ? handleDeleteButton(pokemonData.id)
+            : handleAddButton(pokemonData);
         }}
       >
         {isSelect ? "삭제" : "추가"}
--- src/context/PokemonContext.jsx
+++ src/context/PokemonContext.jsx
@@ -11,8 +11,42 @@ export const PokemonProvider = ({ children }) => {
     null,
     null,
   ]);
+  const handleAddButton = (pokemonData) => {
+    const dataLength = selectedPokemon.filter((e) => e !== null).length;
+    if (dataLength >= 6) {
+      alert("선택할 수 있는 포켓몬 수를 넘었습니다.");
+      return;
+    }
+    if (selectedPokemon.includes(pokemonData)) {
+      alert("이미 선택한 포켓몬입니다.");
+      return;
+    }
+    const newArr = [...selectedPokemon];
+    const nullIndex = newArr.indexOf(null);
+    if (nullIndex !== -1) {
+      newArr[nullIndex] = pokemonData;
+    }
+    setSelectedPokemon(newArr);
+  };
+
+  const handleDeleteButton = (id) => {
+    const filteredData = selectedPokemon.filter(
+      (e) => e !== null && e.id !== id
+    );
+
+    while (filteredData.length < selectedPokemon.length) {
+      filteredData.push(null);
+    }
+    setSelectedPokemon(filteredData);
+  };
   return (
-    <PokemonContext.Provider value={{ selectedPokemon, setSelectedPokemon }}>
+    <PokemonContext.Provider
+      value={{
+        selectedPokemon,
+        handleAddButton,
+        handleDeleteButton,
+      }}
+    >
       {children}
     </PokemonContext.Provider>
   );
